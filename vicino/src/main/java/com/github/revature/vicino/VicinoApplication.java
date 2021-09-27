74c7ac8bf892e3a05d8b5e53e2e0d98a1410cbeb
Andrew Gregersen <andrew.gregersen@revature.net>
2021-09-27 09:17:49 -0700

    Initial Commit

---
+++ vicino/src/main/java/com/github/revature/vicino/VicinoApplication.java
@@ -0,0 +1,13 @@
+package com.github.revature.vicino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VicinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VicinoApplication.class, args);
+	}
+
+}
